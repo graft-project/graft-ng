eebdec1aa9b217df1d137aba0fa2a161d95c6e13
Alexander Suprunenko <alexander.suprunenko@graft.network>
2018-06-22 19:14:39 +0300

    after review. code formatting fixed.

--- include/mongoosex.h
+++ include/mongoosex.h
@@ -9,13 +9,14 @@
 
 namespace mg
 {
-struct mg_connection *mg_connect_http_opt_x(
-    struct mg_mgr *mgr, MG_CB(mg_event_handler_t ev_handler, void *user_data),
-    struct mg_connect_opts opts, const char *url, const char *extra_headers,
+
+mg_connection *mg_connect_http_opt_x(
+    mg_mgr *mgr, MG_CB(mg_event_handler_t ev_handler, void *user_data),
+    mg_connect_opts opts, const char *url, const char *extra_headers,
     const std::string& post_data);
 
-struct mg_connection *mg_connect_http_x(
-    struct mg_mgr *mgr,
+mg_connection *mg_connect_http_x(
+    mg_mgr *mgr,
     MG_CB(mg_event_handler_t event_handler, void *user_data), const char *url,
     const char *extra_headers, const std::string& post_data);
 
--- src/mongoosex.cpp
+++ src/mongoosex.cpp
@@ -1,65 +1,65 @@
 #include "mongoosex.h"
 
-#ifdef __cplusplus
 extern "C" {
-#endif /* __cplusplus */
 
-struct mg_connection *mg_connect_http_base(
-    struct mg_mgr *mgr, MG_CB(mg_event_handler_t ev_handler, void *user_data),
-    struct mg_connect_opts opts, const char *scheme1, const char *scheme2,
+mg_connection *mg_connect_http_base(
+    mg_mgr *mgr, MG_CB(mg_event_handler_t ev_handler, void *user_data),
+    mg_connect_opts opts, const char *scheme1, const char *scheme2,
     const char *scheme_ssl1, const char *scheme_ssl2, const char *url,
-    struct mg_str *path, struct mg_str *user_info, struct mg_str *host);
+    mg_str *path, mg_str *user_info, mg_str *host);
 
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
+} //extern "C"
 
 namespace mg
 {
 
-struct mg_connection *mg_connect_http_opt_x(
-    struct mg_mgr *mgr, MG_CB(mg_event_handler_t ev_handler, void *user_data),
-    struct mg_connect_opts opts, const char *url, const char *extra_headers,
-    const std::string& post_data) {
-  struct mg_str user = MG_NULL_STR, null_str = MG_NULL_STR;
-  struct mg_str host = MG_NULL_STR, path = MG_NULL_STR;
-  struct mbuf auth;
-  struct mg_connection *nc =
-      mg_connect_http_base(mgr, MG_CB(ev_handler, user_data), opts, "http",
-                           NULL, "https", NULL, url, &path, &user, &host);
+mg_connection *mg_connect_http_opt_x(
+    mg_mgr *mgr, MG_CB(mg_event_handler_t ev_handler, void *user_data),
+    mg_connect_opts opts, const char *url, const char *extra_headers,
+    const std::string& post_data)
+{
+    mg_str user = MG_NULL_STR, null_str = MG_NULL_STR;
+    mg_str host = MG_NULL_STR, path = MG_NULL_STR;
+    mbuf auth;
+    mg_connection *nc = mg_connect_http_base(
+                mgr, MG_CB(ev_handler, user_data),
+                opts, "http", NULL,
+                "https", NULL, url,
+                &path, &user, &host);
 
-  if (nc == NULL) {
-    return NULL;
-  }
+    if (nc == NULL) return NULL;
 
-  mbuf_init(&auth, 0);
-  if (user.len > 0) {
-    mg_basic_auth_header(user, null_str, &auth);
-  }
+    mbuf_init(&auth, 0);
+    if (user.len > 0)
+    {
+        mg_basic_auth_header(user, null_str, &auth);
+    }
 
-  if (extra_headers == NULL) extra_headers = "";
-  if (path.len == 0) path = mg_mk_str("/");
-  if (host.len == 0) host = mg_mk_str("");
+    if (extra_headers == NULL) extra_headers = "";
+    if (path.len == 0) path = mg_mk_str("/");
+    if (host.len == 0) host = mg_mk_str("");
 
-  mg_printf(nc, "%s %.*s HTTP/1.1\r\nHost: %.*s\r\nContent-Length: %" SIZE_T_FMT
-                "\r\n%.*s%s\r\n",
-            (post_data.empty() ? "GET" : "POST"), (int) path.len, path.p,
-            (int) (path.p - host.p), host.p, post_data.size(), (int) auth.len,
-            (auth.buf == NULL ? "" : auth.buf), extra_headers);
+    mg_printf(nc, "%s %.*s HTTP/1.1\r\nHost: %.*s\r\nContent-Length: %" SIZE_T_FMT
+              "\r\n%.*s%s\r\n",
+              (post_data.empty() ? "GET" : "POST"), (int) path.len, path.p,
+              (int) (path.p - host.p), host.p, post_data.size(), (int) auth.len,
+              (auth.buf == NULL ? "" : auth.buf), extra_headers);
 
-  mg_send(nc, post_data.c_str(), post_data.size());
+    mg_send(nc, post_data.c_str(), post_data.size());
 
-  mbuf_free(&auth);
-  return nc;
+    mbuf_free(&auth);
+    return nc;
 }
 
-struct mg_connection *mg_connect_http_x(
-    struct mg_mgr *mgr, MG_CB(mg_event_handler_t ev_handler, void *user_data),
-    const char *url, const char *extra_headers, const std::string& post_data) {
-  struct mg_connect_opts opts;
-  memset(&opts, 0, sizeof(opts));
-  return mg_connect_http_opt_x(mgr, MG_CB(ev_handler, user_data), opts, url,
-                             extra_headers, post_data);
+mg_connection *mg_connect_http_x(
+    mg_mgr *mgr, MG_CB(mg_event_handler_t ev_handler, void *user_data),
+    const char *url, const char *extra_headers, const std::string& post_data)
+{
+    mg_connect_opts opts;
+    memset(&opts, 0, sizeof(opts));
+    return mg_connect_http_opt_x(mgr, MG_CB(ev_handler, user_data),
+                                 opts, url, extra_headers,
+                                 post_data);
 }
 
 } //namespace mg
