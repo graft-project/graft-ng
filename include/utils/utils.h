f90f387e02a025637edf4ec2758cda304ee9f684
Ilya Kitaev <mbg033@gmail.com>
2018-07-06 20:09:36 +0300

    fixed build error

--- include/utils/utils.h
+++ include/utils/utils.h
@@ -6,8 +6,8 @@
 namespace graft {
 namespace utils {
 
-static std::string base64_decode(const std::string &encoded_data);
-static std::string base64_encode(const std::string &data);
+std::string base64_decode(const std::string &encoded_data);
+std::string base64_encode(const std::string &data);
 
 }
 }
--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -3,14 +3,15 @@
 namespace graft {
 namespace utils {
 
-static std::string base64_decode(const std::string &encoded_data)
+std::string base64_decode(const std::string &encoded_data)
 {
     return "";
 }
 
-static std::string base64_encode(const std::string &data)
+std::string base64_encode(const std::string &data)
 {
     return "";
 }
 
+}
 }
