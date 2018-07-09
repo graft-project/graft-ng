1e318ac3de966fc5cab558394fea0722846a53f1
Ilya Kitaev <mbg033@gmail.com>
2018-07-09 21:14:03 +0300

    add: base64_decode, base64_encode using epee implementation

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -1,16 +1,19 @@
 #include "utils/utils.h"
+#include <locale>            // epee::string_coding uses std::locale but misses include
+#include <string_coding.h>
+
 
 namespace graft {
 namespace utils {
 
 std::string base64_decode(const std::string &encoded_data)
 {
-    return "";
+    return epee::string_encoding::base64_decode(encoded_data);
 }
 
 std::string base64_encode(const std::string &data)
 {
-    return "";
+    return epee::string_encoding::base64_encode(data);
 }
 
 }
