4af947806112c5e9c9c9239ab1bf8e136a88f3e2
Ilya Kitaev <mbg033@gmail.com>
2018-07-23 17:19:09 +0300

    Error responsea are json-rpc now

--- src/requestdefines.cpp
+++ src/requestdefines.cpp
@@ -1,32 +1,39 @@
 #include "requestdefines.h"
+#include "jsonrpc.h"
 
 
 namespace graft {
 
 Status errorInvalidPaymentID(Output &output)
 {
-    ErrorResponse err;
+    JsonRpcError err;
     err.code = ERROR_PAYMENT_ID_INVALID;
     err.message = MESSAGE_PAYMENT_ID_INVALID;
-    output.load(err);
+    JsonRpcErrorResponse resp;
+    resp.error = err;
+    output.load(resp);
     return Status::Error;
 }
 
 Status errorInvalidParams(Output &output)
 {
-    ErrorResponse err;
+    JsonRpcError err;
     err.code = ERROR_INVALID_PARAMS;
     err.message = MESSAGE_INVALID_PARAMS;
-    output.load(err);
+    JsonRpcErrorResponse resp;
+    resp.error = err;
+    output.load(resp);
     return Status::Error;
 }
 
 Status errorInvalidAmount(Output &output)
 {
-    ErrorResponse err;
+    JsonRpcError err;
     err.code = ERROR_AMOUNT_INVALID;
     err.message = MESSAGE_AMOUNT_INVALID;
-    output.load(err);
+    JsonRpcErrorResponse resp;
+    resp.error = err;
+    output.load(resp);
     return Status::Error;
 }
 
@@ -35,7 +42,7 @@ bool errorFinishedPayment(int status, Output &output)
     if (status != static_cast<int>(RTAStatus::Waiting)
             && status != static_cast<int>(RTAStatus::InProgress))
     {
-        ErrorResponse err;
+        JsonRpcError err;
         if (status == static_cast<int>(RTAStatus::Success))
         {
             err.code = ERROR_RTA_COMPLETED;
@@ -46,6 +53,8 @@ bool errorFinishedPayment(int status, Output &output)
             err.code = ERROR_RTA_FAILED;
             err.message = MESSAGE_RTA_FAILED;
         }
+        JsonRpcErrorResponse resp;
+        resp.error = err;
         output.load(err);
         return true;
     }
