3083f076748978ca2520a7dcb4680459c207350e
Alexander Suprunenko <alexander.suprunenko@graft.network>
2018-08-03 17:25:04 +0300

    Terminate handler added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,43 @@
 #include "server.h"
+#include "backtrace.h"
+
+namespace graft
+{
+
+std::terminate_handler prev_terminate = nullptr;
+
+void terminate()
+{
+    std::cerr << "\nTerminate called, dump stack:\n";
+    graft_bt();
+
+    //dump exception info
+    std::exception_ptr eptr = std::current_exception();
+    if(eptr)
+    {
+        try
+        {
+             std::rethrow_exception(eptr);
+        }
+        catch(std::exception& ex)
+        {
+            std::cerr << "\nTerminate caused by exception : '" << ex.what() << "'\n";
+        }
+        catch(...)
+        {
+            std::cerr << "\nTerminate caused by unknown exception.\n";
+        }
+    }
+
+    prev_terminate();
+}
+
+} //namespace graft
 
 int main(int argc, const char** argv)
 {
+    graft::prev_terminate = std::set_terminate( graft::terminate );
+
     try
     {
         graft::GraftServer gserver;
