56807e78861100ac4547bd55f3176ec8c0ee2afc
Alexander Suprunenko <alexander.suprunenko@graft.network>
2019-02-06 03:06:01 +0200

    split function added.

--- src/lib/graft/common/utils.cpp
+++ src/lib/graft/common/utils.cpp
@@ -16,5 +16,33 @@ std::string base64_encode(const std::string &data)
     return epee::string_encoding::base64_encode(data);
 }
 
+namespace {
+template<typename res>
+bool split_impl(const std::string_view& in, char delim, res& first, res& second)
+{
+    size_t pos = in.find(delim);
+    if(pos != std::string::npos)
+    {
+        first = in.substr(0, pos);
+        second = in.substr(pos+1);
+        return true;
+    }
+    first = in;
+    second = std::string_view();
+    return false;
 }
+
+} //namespace
+
+bool split(const std::string_view& in, char delim, std::string_view& first, std::string_view& second)
+{
+    return split_impl(in, delim, first, second);
 }
+
+bool split(const std::string_view& in, char delim, std::string& first, std::string& second)
+{
+    return split_impl(in, delim, first, second);
+}
+
+} //namespace utils
+} //namespace graft
