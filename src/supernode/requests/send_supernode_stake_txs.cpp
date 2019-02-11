a0b3fab4cfca94d59a69ca96ee849bb6b75cbb04
Leny Kholodov <leny.kholodov@graft.network>
2019-02-12 01:58:12 +0200

    Adding supernode signature to stake transaction

--- src/supernode/requests/send_supernode_stake_txs.cpp
+++ src/supernode/requests/send_supernode_stake_txs.cpp
@@ -46,6 +46,28 @@ namespace graft::supernode::request {
 namespace
 {
 
+bool verifySignature(const std::string& id_key, const std::string& wallet_public_address, const std::string& signature)
+{
+    crypto::public_key W;
+    if (!epee::string_tools::hex_to_pod(id_key, W))
+    {
+        LOG_ERROR("Invalid supernode public identifier '" << id_key << "'");
+        return false;
+    }
+
+    crypto::signature sign;
+    if (!epee::string_tools::hex_to_pod(signature, sign))
+    {
+        LOG_ERROR("Invalid supernode signature '" << signature << "'");
+        return false;
+    }
+
+    std::string data = wallet_public_address + ":" + id_key;
+    crypto::hash hash;
+    crypto::cn_fast_hash(data.data(), data.size(), hash);
+    return crypto::check_signature(hash, W, sign);
+}
+
 Status supernodeStakeTransactionsHandler
  (const Router::vars_t& vars,
   const graft::Input& input,
@@ -92,7 +114,12 @@ Status supernodeStakeTransactionsHandler
             return Status::Error;
         }
 
-        //TODO: should check supernode == sn?
+        if (!verifySignature(tx.supernode_public_id, tx.supernode_public_address, tx.supernode_signature))
+        {
+           LOG_ERROR("Supernode signature failed for supernode_public_id='" << tx.supernode_public_id <<"', supernode_public_address='" <<
+               tx.supernode_public_address << "', signature='" << tx.supernode_signature << "'");
+           return Status::Error;
+        }
 
         sn->setStakeTransactionBlockHeight(tx.block_height);
         sn->setStakeTransactionUnlockTime(tx.unlock_time);
