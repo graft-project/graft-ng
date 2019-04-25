0c528d7a0db816adcda33420acd8fdacf58e5abf
Alexander Suprunenko <alexander.suprunenko@graft.network>
2019-04-25 18:30:56 +0300

    Binary serialization support

---
+++ include/lib/graft/binary_serialize.h
@@ -0,0 +1,221 @@
+// Copyright (c) 2018, The Graft Project
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are
+// permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list
+//    of conditions and the following disclaimer in the documentation and/or other
+//    materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors may be
+//    used to endorse or promote products derived from this software without specific
+//    prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+// THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#pragma once
+
+#include <boost/hana.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+
+#include "lib/graft/inout.h"
+
+namespace graft { namespace serializer {
+
+namespace binary_details
+{
+
+template<typename ...>
+using to_void = void; // maps everything to void, used in non-evaluated contexts
+
+template<typename C, typename = void>
+struct is_container : std::false_type
+{};
+
+template<typename C>
+struct is_container<C,
+        to_void<decltype(std::declval<C>().begin()),
+                decltype(std::declval<C>().end()),
+                typename C::value_type
+        >> : std::true_type // will  be enabled for iterable objects
+{};
+
+static_assert( is_container<std::string>::value );
+static_assert( is_container<std::vector<int>>::value );
+///////////////////////
+/*! \brief write_varint adopted from cryptonode.
+ */
+template<typename Arch, typename V>
+// Requires T to be both an integral type and unsigned, should be a compile error if it is not
+static void write_varint(Arch& ar, V i) {
+  // Make sure that there is one after this
+  while (i >= 0x80) {
+    char ch = (static_cast<char>(i) & 0x7f) | 0x80;
+//        ++dest;
+    ar << ch;
+    i >>= 7;			// I should be in multiples of 7, this should just get the next part
+  }
+  // writes the last one to dest
+/*
+  *dest = static_cast<char>(i);
+  dest++;			// Seems kinda pointless...
+*/
+  ar << static_cast<char>(i);
+}
+
+
+/*! \brief read_varint adopted from cryptonode.
+ */
+template<typename Arch, typename V>
+static bool read_varint(Arch& ar, V& write)
+{
+  constexpr int bits = std::numeric_limits<V>::digits;
+  int read = 0;
+  write = 0;
+  for (int shift = 0;; shift += 7) {
+/*
+    if (first == last)
+    {
+        return read;
+    }
+*/
+/*
+    unsigned char byte = *first;
+    ++first;
+*/
+    unsigned char byte;
+    ar >> byte;
+
+    ++read;
+
+    if (shift + 7 >= bits && byte >= 1 << (bits - shift)) {
+  return -1; //EVARINT_OVERFLOW;
+    }
+    if (byte == 0 && shift != 0) {
+  return -2; //EVARINT_REPRESENT;
+    }
+
+    write |= static_cast<V>(byte & 0x7f) << shift; /* Does the actualy placing into write, stripping the first bit */
+
+    /* If there is no next */
+    if ((byte & 0x80) == 0) {
+  break;
+    }
+  }
+  return read;
+}
+
+//////////////
+template<typename Arch, typename V>
+static void ser(Arch& ar, V& v)
+{
+    if constexpr(is_container<V>::value)
+    {
+        size_t size = v.size();
+        write_varint(ar, size);
+        std::for_each(v.begin(), v.end(), [&](auto& item)
+        {
+            ser(ar, item);
+        });
+    }
+    else
+    {
+        ar << v;
+    }
+}
+
+template<typename Arch, typename V>
+static void deser(Arch& ar, V& v)
+{
+    if constexpr(is_container<V>::value)
+    {
+        size_t size;
+        read_varint(ar, size);
+        v.resize(size);
+        std::for_each(v.begin(), v.end(), [&](auto& item)
+        {
+            deser(ar, item);
+        });
+    }
+    else
+    {
+        ar >> v;
+    }
+}
+
+template<typename Arch, typename V>
+static void serialize(Arch& ar, V& v)
+{
+    boost::hana::for_each(boost::hana::keys(v), [&](auto key)
+    {
+        const auto& member = boost::hana::at_key(v, key);
+        using naked_member_t = std::remove_cv_t<std::remove_reference_t<decltype(member)>>;
+        if constexpr(!std::is_base_of<ReflectiveRapidJSON::JsonSerializable<naked_member_t>, naked_member_t>::value)
+        {
+            ser(ar, member);
+        }
+        else
+        {
+            serialize(ar, member);
+        }
+    });
+}
+
+template<typename Arch, typename V>
+static void deserialize(Arch& ar, V& v)
+{
+    boost::hana::for_each(boost::hana::keys(v), [&](auto key)
+    {
+        auto& member = boost::hana::at_key(v, key);
+        using naked_member_t = std::remove_cv_t<std::remove_reference_t<decltype(member)>>;
+        if constexpr(!std::is_base_of<ReflectiveRapidJSON::JsonSerializable<naked_member_t>, naked_member_t>::value)
+        {
+            deser(ar, member);
+        }
+        else
+        {
+            deserialize(ar, member);
+        }
+    });
+}
+
+} //namespace binary_details
+
+//It is expected that serialize and deserialize throw exception on error
+template<typename T>
+class Binary
+{
+public:
+    static std::string serialize(const T& t)
+    {
+        std::ostringstream oss;
+        boost::archive::binary_oarchive ar(oss, boost::archive::no_header);
+        binary_details::serialize(ar, t);
+        return oss.str();
+    }
+
+    static void deserialize(const std::string& s, T& t)
+    {
+        std::istringstream iss(s);
+        boost::archive::binary_iarchive ar(iss, boost::archive::no_header);
+        binary_details::deserialize(ar,t);
+    }
+};
+
+} } //namespace graft { namespace serializer
+
+
---
+++ test/serialize_test.cpp
@@ -0,0 +1,257 @@
+// Copyright (c) 2018, The Graft Project
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are
+// permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list
+//    of conditions and the following disclaimer in the documentation and/or other
+//    materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors may be
+//    used to endorse or promote products derived from this software without specific
+//    prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+// THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include <gtest/gtest.h>
+
+#include "lib/graft/binary_serialize.h"
+
+#include "include_base_utils.h"
+using namespace epee;
+
+#include "cryptonote_basic/cryptonote_format_utils.h"
+#include "serialization/binary_utils.h"
+
+#include "lib/graft/jsonrpc.h"
+
+namespace
+{
+GRAFT_DEFINE_IO_STRUCT(Payment,
+      (uint64, amount),
+      (uint32, block_height),
+      (std::string, payment_id),
+      (std::string, tx_hash),
+      (uint32, unlock_time)
+);
+
+GRAFT_DEFINE_IO_STRUCT(NestedPayment,
+      (std::string, s),
+      (Payment, p),
+      (uint32, i)
+);
+
+GRAFT_DEFINE_IO_STRUCT(WithVector,
+      (std::vector<std::string>, v)
+);
+
+//TODO: does not work
+GRAFT_DEFINE_IO_STRUCT(WithVectorInt,
+      (std::vector<uint32>, v)
+);
+
+struct PaymentX
+{
+  uint64 amount;
+  uint32 block_height;
+  std::string payment_id;
+  std::string tx_hash;
+  uint32 unlock_time;
+  BEGIN_SERIALIZE()
+    FIELD(amount)
+    FIELD(block_height)
+    FIELD(payment_id)
+    FIELD(tx_hash)
+    FIELD(unlock_time)
+  END_SERIALIZE()
+};
+
+struct NestedPaymentX
+{
+  std::string s;
+  PaymentX p;
+  uint32 i;
+  BEGIN_SERIALIZE()
+    FIELD(s)
+    FIELD(p)
+    FIELD(i)
+  END_SERIALIZE()
+};
+
+struct WithVectorX
+{
+  std::vector<std::string> v;
+  BEGIN_SERIALIZE()
+    FIELD(v)
+  END_SERIALIZE()
+};
+
+//TODO: does not work
+struct WithVectorIntX
+{
+  std::vector<uint32> v;
+  BEGIN_SERIALIZE()
+    FIELD(v)
+  END_SERIALIZE()
+};
+
+} //namespace
+
+namespace graft { namespace serializer {
+
+template<typename T>
+struct Nothing
+{
+    static std::string serialize(const T& t)
+    {
+        return "";
+    }
+    static void deserialize(const std::string& s, T& t)
+    {
+    }
+};
+
+} } //namespace graft { namespace serializer
+
+TEST(InOut, serialization)
+{
+    using namespace graft;
+    namespace serial = graft::serializer;
+
+    GRAFT_DEFINE_IO_STRUCT(J,
+        (int,x),
+        (int,y)
+    );
+
+    J j;
+
+    Input input;
+    input.body = "{\"x\":1,\"y\":2}";
+        j.x = 5; j.y = 6;
+    j = input.get<J, serial::JSON<J>>();
+    j = input.get<J>();
+        EXPECT_EQ(j.x, 1); EXPECT_EQ(j.y, 2);
+    j = input.get<J>();
+        EXPECT_EQ(j.x, 1); EXPECT_EQ(j.y, 2);
+        j.x = 5; j.y = 6;
+    j = input.getT<serial::JSON, J>();
+//    j = input.getT<J>();
+        EXPECT_EQ(j.x, 1); EXPECT_EQ(j.y, 2);
+
+    Output output;
+    output.load<J, serial::JSON<J>>(j);
+    output.load<J>(j);
+    output.load<>(j);
+        EXPECT_EQ(input.body, output.body);
+        output.body.clear();
+    output.load(j);
+        EXPECT_EQ(input.body, output.body);
+
+    output.body.clear();
+    output.loadT<serial::JSON, J>(j);
+    output.loadT<serial::JSON>(j);
+    output.loadT<>(j);
+        EXPECT_EQ(input.body, output.body);
+        output.body.clear();
+    output.loadT(j);
+        EXPECT_EQ(input.body, output.body);
+
+    struct A
+    {
+        int x;
+        int y;
+    };
+
+    A a;
+
+    a = input.get<A, serial::Nothing<A>>();
+    a = input.getT<serial::Nothing, A>();
+    output.load<A, serial::Nothing<A>>(a);
+    output.loadT<serial::Nothing, A>(a);
+    output.loadT<serial::Nothing>(a);
+
+    {
+        output.load<J, serial::Binary<J>>(j);
+        std::string s = output.body;
+        input.body = s;
+        J j1;
+        j1 = input.getT<serial::Binary, J>();
+        EXPECT_EQ(j.x, j1.x);
+        EXPECT_EQ(j.y, j1.y);
+    }
+
+    Payment p{ {}, 1, 2, "abc", "defg", 5 };
+    output.load<Payment, serial::Binary<Payment>>(p);
+    PaymentX px{ 1, 2, "abc", "defg", 5 };
+    std::string blob;
+    ::serialization::dump_binary(px, blob);
+    EXPECT_EQ(output.body, blob);
+
+    Payment p1;
+    input.body = output.body;
+    p1 = input.get<Payment, serial::Binary<Payment>>();
+    EXPECT_EQ(p.amount, p1.amount);
+    EXPECT_EQ(p.block_height, p1.block_height);
+    EXPECT_EQ(p.payment_id, p1.payment_id);
+    EXPECT_EQ(p.tx_hash, p1.tx_hash);
+    EXPECT_EQ(p.unlock_time, p1.unlock_time);
+
+    {
+        NestedPayment np{ {}, "something", p, 123 };
+        output.load<NestedPayment, serial::Binary<NestedPayment>>(np);
+        NestedPaymentX npx{ "something", px, 123 };
+    //    blob.clear();
+        ::serialization::dump_binary(npx, blob);
+        EXPECT_EQ(output.body, blob);
+
+        NestedPayment np1;
+        input.body = output.body;
+        np1 = input.get<NestedPayment, graft::serializer::Binary<NestedPayment>>();
+        EXPECT_EQ(np.s, np1.s);
+        EXPECT_EQ(np.p.amount, np1.p.amount);
+        EXPECT_EQ(np.p.block_height, np1.p.block_height);
+        EXPECT_EQ(np.p.payment_id, np1.p.payment_id);
+        EXPECT_EQ(np.p.tx_hash, np1.p.tx_hash);
+        EXPECT_EQ(np.p.unlock_time, np1.p.unlock_time);
+        EXPECT_EQ(np.i, np1.i);
+    }
+    {
+        WithVector wv{ {}, {"1","2","3","4","5"} };
+        output.load<WithVector, graft::serializer::Binary<WithVector>>(wv);
+        WithVectorX wvx{ {"1","2","3","4","5"} };
+        ::serialization::dump_binary(wvx, blob);
+        EXPECT_EQ(output.body, blob);
+
+        input.body = output.body;
+        WithVector wv1 = input.get<WithVector, graft::serializer::Binary<WithVector>>();
+        EXPECT_EQ(wv.v, wv1.v);
+    }
+//does not work yet
+#if 0
+    {
+        WithVectorInt wv{ {}, {1,2,3,4,5} };
+        output.load<WithVectorInt, graft::serializer::Binary<WithVectorInt>>(wv);
+        WithVectorIntX wvx{ {1,2,3,4,5} };
+        ::serialization::dump_binary(wvx, blob);
+        EXPECT_EQ(output.body, blob);
+
+        input.body = output.body;
+        WithVectorInt wv1 = input.get<WithVectorInt, graft::serializer::Binary<WithVectorInt>>();
+        EXPECT_EQ(wv.v, wv1.v);
+    }
+#endif
+}
+
